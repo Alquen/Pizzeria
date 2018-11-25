ec5b55be0bef8e67ba247e6c60877e4011f68da0
alque <alque@NecroAny>
2018-11-24 18:01:45 -0600

    Venta Clases

--- PizzeriaProyecto/src/PizzeriaDAL/ConexionDAL.java
+++ PizzeriaProyecto/src/PizzeriaDAL/ConexionDAL.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PizzeriaDAL;
 
-/**
- *
- * @author alque
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ConexionDAL {
     
+    String CadenaConexion="jdbc:sqlite:C:\\Users\\alque\\Desktop\\7D\\Programacion\\practica3.db";
+    Connection conn=null;
+    
+    public ConexionDAL(){
+    
+    }
+    public Connection Conectar(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            this.conn= DriverManager.getConnection(CadenaConexion);
+        } catch (Exception e) {
+            System.out.println("Problemas al conectarse"+e);
+        }
+      return this.conn;
+    }
+    
+    public int probar(){
+        try{
+              Class.forName("org.sqlite.JDBC");
+              Connection c = DriverManager.getConnection(CadenaConexion);
+        }catch (Exception e){
+            return 0;
+        }
+        return 1;
+    }
+    
+    public void Desconectar(){
+        this.conn=null;
+    }
+    
+   
+    public int EjecutarComandoSQL(String Sentencia){
+        try {
+         PreparedStatement pstm=Conectar().prepareStatement(Sentencia);
+         pstm.execute();
+         return 1;
+        }catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        }
+    
+    }
+    public ResultSet EjecutarSentenciaSQL(String Sentencia){
+     try {
+         PreparedStatement pstm=Conectar().prepareStatement(Sentencia);
+         pstm.execute();
+          ResultSet Resultado=pstm.executeQuery();
+           return Resultado;
+        }catch (SQLException e) {
+            System.out.println(e);
+         return null;   
+        }
+    }
 }
