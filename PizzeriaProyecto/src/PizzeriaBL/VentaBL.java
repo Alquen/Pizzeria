e6dc0f344b36061a9dfbb88d9c6e222cb92d02c1
alque <alque@NecroAny>
2018-11-24 19:40:51 -0600

    menu dosnnn

--- PizzeriaProyecto/src/PizzeriaBL/VentaBL.java
+++ PizzeriaProyecto/src/PizzeriaBL/VentaBL.java
@@ -10,5 +10,67 @@ package PizzeriaBL;
  * @author alque
  */
 public class VentaBL {
-    
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getFolio() {
+        return Folio;
+    }
+
+    public void setFolio(String Folio) {
+        this.Folio = Folio;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    public int getId_pizza() {
+        return Id_pizza;
+    }
+
+    public void setId_pizza(int Id_pizza) {
+        this.Id_pizza = Id_pizza;
+    }
+
+    public int getId_comprador() {
+        return Id_comprador;
+    }
+
+    public void setId_comprador(int Id_comprador) {
+        this.Id_comprador = Id_comprador;
+    }
+
+    public int getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(int Cantidad) {
+        this.Cantidad = Cantidad;
+    }
+    public int Id;
+    public String Folio;
+    public String Fecha;
+    public int Id_pizza;
+    public int Id_comprador;
+    public int Cantidad;
+
+    public int getTotal() {
+        return Total;
+    }
+
+    public void setTotal(int Total) {
+        this.Total = Total;
+    }
+    public int Total;
 }
--- PizzeriaProyecto/src/PizzeriaDAL/VentaDAL.java
+++ PizzeriaProyecto/src/PizzeriaDAL/VentaDAL.java
@@ -4,11 +4,47 @@
  * and open the template in the editor.
  */
 package PizzeriaDAL;
+import PizzeriaBL.VentaBL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author alque
  */
 public class VentaDAL {
+    
+    ConexionDAL conex = new ConexionDAL();
+    
+    public int Cobrar(VentaBL ObjVenta){
+        int Comando = conex.EjecutarComandoSQL("INSERT INTO tblTicket(folio, id_cliente, fecha, total )"+
+                "VALUES ('"+ ObjVenta.getFolio()+"', '"+ ObjVenta.getId_comprador()+"','"+ ObjVenta.getFecha() +"', '"+ ObjVenta.getTotal()+"' )");
+        conex.Desconectar();
+        return Comando;
+    }
+    
+    public int Pizza (VentaBL ObjVenta){
+        int Comando = conex.EjecutarComandoSQL("INSERT INTO tblVenta(folio, id_pizza, cantidad )"+
+                "VALUES ('"+ ObjVenta.getFolio()+"', '"+ ObjVenta.getId_pizza()+"','"+ ObjVenta.getCantidad() +"' )");
+        conex.Desconectar();
+        return Comando;
+    }
+    
+    public String LastFolio(){
+        try{
+            String res = "";
+                ResultSet result = conex.EjecutarSentenciaSQL("");
+            while(result.next()){
+                String[] Folio = {
+                    result.getString(1)
+                };
+                res = Folio[1];
+            }
+           
+             return res; 
+        }catch(SQLException e){
+            return null;
+        }
+    }
     
 }
